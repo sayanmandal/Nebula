2d00cc9a3696ad5d1aef55f59c888422d8fc8886
Sayan Mandal <sayan007roni@gmail.com>
2019-06-02 19:30:48 +0530

    Level 1 cracked

---
+++ Level01.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <stdio.h>
+
+int main(int argc, char **argv, char **envp)
+{
+  gid_t gid;
+  uid_t uid;
+  gid = getegid();
+  uid = geteuid();
+
+  setresgid(gid, gid, gid);
+  setresuid(uid, uid, uid);
+
+  system("/usr/bin/env echo and now what?");
+}
+
+
+/*
+  solution:
+  > PATH = /tmp:$PATH
+  > ln -s /bin/getflag /tmp/echo
+  > ./getflag 
+*/
